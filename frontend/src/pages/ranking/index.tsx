510221eefb5fc42ee29eaf879437c5c0c1297715
KitaharaMugiro <you@example.com>
2022-10-10 19:50:43 +0900

    jsonからブロックチェーンに置き換え

--- frontend/src/pages/ranking/index.tsx
+++ frontend/src/pages/ranking/index.tsx
@@ -3,15 +3,17 @@ import { css } from "@emotion/react";
 
 import { TableSort } from "@/components/TableSort";
 import attributes from "@/components/TableSort/attributes.json";
+import useDaoHistory from "@/hooks/dao/useDaoHistory";
 
 const Ranking: NextPage = () => {
+  const { daoHistory } = useDaoHistory()
   return (
     <div
       css={css`
         margin: 32px 300px;
       `}
     >
-      <TableSort data={attributes.props.data} />
+      <TableSort data={daoHistory} />
     </div>
   );
 };
