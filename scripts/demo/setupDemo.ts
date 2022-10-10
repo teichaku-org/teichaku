18c4ec2d8ab527229bde3e4d8018ba6277c62416
KitaharaMugiro <you@example.com>
2022-10-10 19:23:58 +0900

    デモデータの追加

--- scripts/demo/setupDemo.ts
+++ scripts/demo/setupDemo.ts
@@ -2,6 +2,7 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { ethers } from "ethers";
 import { DAOToken, DAONFT, DAOHistory } from "../../typechain-types";
 import { DAOHistoryItemStruct } from "../../typechain-types/contracts/DAOHistory";
+import createDaoHistory from "./createDaoHistory";
 
 async function setupDemo(token: DAOToken, owner: SignerWithAddress, otherAccount: SignerWithAddress, daonft: DAONFT, otherAccount2: SignerWithAddress, daoHistory: DAOHistory) {
     // Tokenの発行
@@ -14,47 +15,7 @@ async function setupDemo(token: DAOToken, owner: SignerWithAddress, otherAccount
     await daonft.safeMint(otherAccount2.address);
 
     // DaoHistoryの追加
-    const daoHistoryItem1: DAOHistoryItemStruct = {
-        contributionText: "Web3Hachathonの準備",
-        reward: ethers.utils.parseEther("100"),
-        roles: ["owner", "engineer"],
-        timestamp: 1665390549,
-        contributor: owner.address,
-        pollId: 0,
-        score: {
-            scores: [3, 4, 2],
-            perspectiveId: 1
-        }
-    };
-    await daoHistory.addDaoHistory("demo", 0, daoHistoryItem1);
-
-    const daoHistoryItem2: DAOHistoryItemStruct = {
-        contributionText: "フロントエンドの実装",
-        reward: ethers.utils.parseEther("50"),
-        roles: ["engineer", "designer"],
-        timestamp: 1665390549,
-        contributor: otherAccount.address,
-        pollId: 0,
-        score: {
-            scores: [2, 2, 3],
-            perspectiveId: 1
-        }
-    };
-    await daoHistory.addDaoHistory("demo", 0, daoHistoryItem2);
-
-    const daoHistoryItem3: DAOHistoryItemStruct = {
-        contributionText: "DAOについて調査",
-        reward: ethers.utils.parseEther("150"),
-        roles: [],
-        timestamp: 1665390549,
-        contributor: otherAccount2.address,
-        pollId: 0,
-        score: {
-            scores: [5, 5, 5],
-            perspectiveId: 1
-        }
-    };
-    await daoHistory.addDaoHistory("demo", 0, daoHistoryItem3);
+    await createDaoHistory(daoHistory)
 
     // TODO: 辻褄を合わせるためにpollIdをインクリメントする
 
