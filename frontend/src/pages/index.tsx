b31d4a1499981078ccd6694714b9df78bced12ce
KitaharaMugiro <you@example.com>
2022-10-13 23:43:04 +0900

    LPをとりあえず仮置きで作成

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,23 +1,11 @@
-import { Button } from "@mantine/core";
-import type { NextPage } from "next";
-import Link from "next/link";
+import { Features } from "@/components/landing-page/Features";
+import { HeroTitle } from "@/components/landing-page/HeroTitle";
 
 const Home = () => {
   return (
     <div>
-      <div>
-        <p>トークン総発行数</p>
-        <Link href="/test" passHref>
-          <Button component="a">トークン総発行数</Button>
-        </Link>
-      </div>
-
-      <div>
-        <p>DAO history</p>
-        <Link href="/history" passHref>
-          <Button component="a">DAO history</Button>
-        </Link>
-      </div>
+      <HeroTitle />
+      <Features />
     </div>
   );
 };
