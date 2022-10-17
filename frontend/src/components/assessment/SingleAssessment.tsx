8a35481dba39e11b7374d54dd892193513e4c22e
KitaharaMugiro <you@example.com>
2022-10-17 22:56:31 +0900

    local string

--- frontend/src/components/assessment/SingleAssessment.tsx
+++ frontend/src/components/assessment/SingleAssessment.tsx
@@ -1,23 +1,16 @@
+import { Assessment } from "@/domains/Assessment"
+import { getSingleAssessment } from "@/utils/analysis/getSingleAssessment"
 import { AssessmentRadar } from "../graphs/AssessmentRadar"
 
-export const SingleAssessment = () => {
-    const data = [
-        {
-            perspective: "Math",
-            You: 120,
-            Average: 48,
-        },
-        {
-            perspective: "Science",
-            You: 110,
-            Average: 38,
-        },
-        {
-            perspective: "English",
-            You: 130,
-            Average: 98,
-        }
-    ]
+interface Props {
+    assessments: Assessment[]
+    perspectives: string[]
+    contributor: string
+    pollId: number
+
+}
+export const SingleAssessment = (props: Props) => {
+    const data = getSingleAssessment(props.assessments, props.perspectives, props.contributor, props.pollId)
     return <div>
         {/* TODO: heightとwidthをウィンドウサイズから設定 */}
         <div style={{ height: 400, width: 400 }}>
--- frontend/src/utils/analysis/getSingleAssessment.ts
+++ frontend/src/utils/analysis/getSingleAssessment.ts
@@ -1,6 +1,23 @@
 import { Assessment } from "@/domains/Assessment";
 import { AssessmentStructOutput } from "@/types/DAOHistory";
 
-export const getSingleAssessment = (assessments: Assessment[], contributor: string, pollId: number) => {
+export const getSingleAssessment = (
+    assessments: Assessment[],
+    perspectives: string[],
+    contributor: string,
+    pollId: number) => {
     const assessment = assessments.find(a => a.contributor === contributor && a.pollId === pollId);
+    const samePollAssessments = assessments.filter(a => a.pollId === pollId);
+    let result = []
+    for (const perspective of perspectives) {
+        const yourPoint = assessment?.points[perspectives.indexOf(perspective)] ?? 0;
+        const averagePoint = samePollAssessments.reduce((acc, cur) => acc + cur.points[perspectives.indexOf(perspective)], 0) / samePollAssessments.length;
+        result.push({
+            perspective,
+            You: yourPoint,
+            Average: averagePoint
+        })
+    }
+
+    return result;
 }
