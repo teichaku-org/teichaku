b1c78b4056a4c1673aaf55ab7ffa6259d064237a
kotakamikawa <kamikawat8@gmail.com>
2022-10-17 09:35:52 +0900

    カードを押したときにサイドバーの開閉を実装

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,72 +2,84 @@ import { AppFooter } from "@/components/common/AppFooter";
 import { AppHeader } from "@/components/common/AppHeader";
 import { AppNavbar } from "@/components/common/AppNavbar";
 import { MetamaskCheck } from "@/components/web3/common/MetamaskCheck";
-import { AppShell, Aside, Container, MantineProvider, MediaQuery, Text } from "@mantine/core";
-import dynamic from 'next/dynamic';
-import NetworkCheck from '../components/web3/common/NetworkCheck';
+import {
+  AppShell,
+  Aside,
+  Container,
+  MantineProvider,
+  MediaQuery,
+  Text,
+} from "@mantine/core";
+import dynamic from "next/dynamic";
+import NetworkCheck from "../components/web3/common/NetworkCheck";
 
 const MyApp = ({ Component, pageProps }: any) => {
-  const SafeHydrate = dynamic(() => import('./SafeHydrage'), { ssr: false });
-
+  const SafeHydrate = dynamic(() => import("./SafeHydrage"), { ssr: false });
 
   const windowErrorRender = () => {
     return (
       <div className="flex flex-col items-center justify-center h-screen">
-        <div className="text-2xl font-bold">このページを開くことはできません(お問い合わせをお願いします)</div>
+        <div className="text-2xl font-bold">
+          このページを開くことはできません(お問い合わせをお願いします)
+        </div>
       </div>
     );
   };
 
   const render = () => {
-    console.log(pageProps.path)
-    return <div>
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: "dark",
-          fontFamily: 'Open Sans, sans serif',
-          shadows: {
-            xs: '-1px 1px 4px #BE4BDB, 1px -1px 4px #228BE6',
-            sm: '-1px 1px 5px 2px #BE4BDB, 2px -2px 5px 2px #228BE6',
-            md: '-3px 3px 5px 2px #BE4BDB, 3px -3px 5px 2px #228BE6',
-            lg: '-5px 5px 10px 3px #BE4BDB, 5px -5px 10px  3px #228BE6',
-            xl: '-8px 8px 70px 5px #BE4BDB, 8px -8px 70px 5px #228BE6',
-          }
-        }}
-
-      >
-        <AppShell
-          padding="md"
-          navbar={!Component.noNavbar ? <AppNavbar /> : undefined}
-          header={<AppHeader />}
-          footer={<AppFooter />}
-          styles={(theme) => ({
-            main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
-          })}
+    console.log(pageProps.path);
+    return (
+      <div>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            colorScheme: "dark",
+            fontFamily: "Open Sans, sans serif",
+            shadows: {
+              xs: "-1px 1px 4px #BE4BDB, 1px -1px 4px #228BE6",
+              sm: "-1px 1px 5px 2px #BE4BDB, 2px -2px 5px 2px #228BE6",
+              md: "-3px 3px 5px 2px #BE4BDB, 3px -3px 5px 2px #228BE6",
+              lg: "-5px 5px 10px 3px #BE4BDB, 5px -5px 10px  3px #228BE6",
+              xl: "-8px 8px 70px 5px #BE4BDB, 8px -8px 70px 5px #228BE6",
+            },
+          }}
         >
-          {/* パスが/である場合は表示する */}
-          {!Component.noNeedWallet && <>
-            <MetamaskCheck />
-            <NetworkCheck />
-          </>}
-          <Container>
+          <AppShell
+            padding="md"
+            navbar={!Component.noNavbar ? <AppNavbar /> : undefined}
+            header={<AppHeader />}
+            footer={<AppFooter />}
+            styles={(theme) => ({
+              main: {
+                backgroundColor:
+                  theme.colorScheme === "dark"
+                    ? theme.colors.dark[8]
+                    : theme.colors.gray[0],
+              },
+            })}
+          >
+            {/* パスが/である場合は表示する */}
+            {!Component.noNeedWallet && (
+              <>
+                <MetamaskCheck />
+                <NetworkCheck />
+              </>
+            )}
             <Component {...pageProps} />
-          </Container>
+          </AppShell>
+        </MantineProvider>
+      </div>
+    );
+  };
 
-        </AppShell>
-      </MantineProvider>
-    </div>
-  }
+  return (
+    <SafeHydrate>
+      <title>DAO History</title>
+      {typeof window === "undefined" ? windowErrorRender() : render()}
+    </SafeHydrate>
+  );
+};
 
-  return <SafeHydrate>
-    <title>DAO History</title>
-    {
-      typeof window === 'undefined' ? windowErrorRender() :
-        render()
-    }
-  </SafeHydrate>
-}
-
-export default MyApp
+export default MyApp;
--- frontend/src/pages/poll/index.tsx
+++ frontend/src/pages/poll/index.tsx
@@ -1,14 +1,21 @@
-import usePoll from "@/hooks/dao/usePoll"
-import { useEffect } from "react"
+import usePoll from "@/hooks/dao/usePoll";
+import { Container } from "@mantine/core";
+import { useEffect } from "react";
 
 const Poll = () => {
-    const { pollDetail } = usePoll()
-    return <div >
+  const { pollDetail } = usePoll();
+  return (
+    <div>
+      <Container>
         <h1>Accepting your votes!</h1>
-        <p>Evaluate the achievements left by DAO members from the following perspectives.</p>
+        <p>
+          Evaluate the achievements left by DAO members from the following
+          perspectives.
+        </p>
 
         {JSON.stringify(pollDetail)}
-
+      </Container>
     </div>
-}
-export default Poll
+  );
+};
+export default Poll;
