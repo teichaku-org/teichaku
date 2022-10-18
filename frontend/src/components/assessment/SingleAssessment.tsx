c38f6d060bc8d1afc4d57ac4a22f8f450c9de02b
KitaharaMugiro <you@example.com>
2022-10-18 23:39:35 +0900

    大体の画面を作った

--- frontend/src/components/assessment/SingleAssessment.tsx
+++ frontend/src/components/assessment/SingleAssessment.tsx
@@ -1,8 +1,10 @@
 import useDaoHistory from "@/hooks/dao/useDaoHistory"
 import usePoll from "@/hooks/dao/usePoll"
 import { getSingleAssessment } from "@/utils/analysis/getSingleAssessment"
+import { Paper, Text } from "@mantine/core"
 import { useEffect, useState } from "react"
 import { AssessmentRadar } from "../graphs/AssessmentRadar"
+import { Comments } from "./Comments"
 
 interface Props {
     contributor: string
@@ -11,9 +13,19 @@ interface Props {
 }
 export const SingleAssessment = (props: Props) => {
     const { fetchPollDetail } = usePoll()
+    const { daoHistory } = useDaoHistory()
     const { assessments } = useDaoHistory()
     const [perspectives, setPerspectives] = useState<string[]>([])
 
+    const contribution = daoHistory.find((item) => item.contributor === props.contributor && item.pollId === props.pollId)
+    const targetAssessments = assessments.filter((item) => item.contributor === props.contributor && item.pollId === props.pollId)
+    const comments = targetAssessments.map((item) => {
+        return {
+            comment: item.comment,
+            author: item.voter,
+            timestamp: contribution?.timestamp || new Date()
+        }
+    })
     useEffect(() => {
         fetchPollDetail(props.pollId).then(res => {
             setPerspectives(res.perspectives)
@@ -22,10 +34,21 @@ export const SingleAssessment = (props: Props) => {
 
 
     const data = getSingleAssessment(assessments, perspectives, props.contributor, props.pollId)
-    return <div>
-        {/* TODO: heightとwidthをウィンドウサイズから設定 */}
-        <div style={{ display: "flex", height: 300, width: 300 }}>
+    return <div >
+        <div style={{ height: 300, width: 300 }}>
             <AssessmentRadar data={data} />
         </div>
+
+        {/* 貢献内容 */}
+        <Paper p="md">
+            <Text size="xs" style={{ whiteSpace: "pre-wrap" }}>
+                {contribution?.contributionText}
+            </Text>
+        </Paper>
+
+        {/* コメント */}
+        <Comments comments={comments} />
+
+        {/* 自分のだったらNFT化 */}
     </div>
 }
