babc1f1510233903f7fec11ef46f4e3bbd1761a5
kotakamikawa <kamikawat8@gmail.com>
2022-10-21 10:09:45 +0900

    個別を作成

--- frontend/src/components/assessment/AssessmentTab.tsx
+++ frontend/src/components/assessment/AssessmentTab.tsx
@@ -1,5 +1,10 @@
 import { Tabs } from "@mantine/core";
-import { IconPhoto, IconMessageCircle } from "@tabler/icons";
+import {
+  IconPhoto,
+  IconMessageCircle,
+  IconChartLine,
+  IconChartPie3,
+} from "@tabler/icons";
 
 import { DaoHistory } from "@/domains/DaoHistory";
 import { HistoryList } from "../history/HistoryList";
@@ -10,20 +15,26 @@ interface Props {
 
 const AssessmentTab = (props: Props) => {
   const { daoHistory } = props;
-  //渡すときに自分だけのリストにしてしまうか
+
+  //TODO: 自分のアドレスを取得する方法が分からなかったので一旦固定値
+  const myDaoHistory = () => {
+    return daoHistory.filter(
+      (dao) => dao.contributor === "0x261f350466E17cbDf9Bc00E2B3875685EF9aB07C"
+    );
+  };
   return (
     <Tabs defaultValue="individual">
       <Tabs.List>
-        <Tabs.Tab value="individual" icon={<IconPhoto size={14} />}>
+        <Tabs.Tab value="individual" icon={<IconChartPie3 size={14} />}>
           個別
         </Tabs.Tab>
-        <Tabs.Tab value="total" icon={<IconMessageCircle size={14} />}>
+        <Tabs.Tab value="total" icon={<IconChartLine size={14} />}>
           集計
         </Tabs.Tab>
       </Tabs.List>
 
       <Tabs.Panel value="individual" pt="xs">
-        <HistoryList data={daoHistory} />
+        <HistoryList data={myDaoHistory()} />
       </Tabs.Panel>
 
       <Tabs.Panel value="total" pt="xs">
