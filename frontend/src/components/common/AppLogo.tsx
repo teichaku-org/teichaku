a87348be86e3dd9e8adbdbebab968a75d586b9ee
kotakamikawa <kamikawat8@gmail.com>
2022-10-22 14:39:00 +0900

    change theme font color white

--- frontend/src/components/common/AppLogo.tsx
+++ frontend/src/components/common/AppLogo.tsx
@@ -1,33 +1,34 @@
-import { createStyles, ThemeIcon } from "@mantine/core";
+import { createStyles, Text, ThemeIcon } from "@mantine/core";
 import { IconCoin } from "@tabler/icons";
 
 const useStyles = createStyles((theme) => ({
-
-    logo: {
-        display: 'flex',
-        alignItems: 'center',
-        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[9],
-        textDecoration: 'none',
-        fontSize: 20,
-        fontWeight: 600,
-    },
-
+  logo: {
+    display: "flex",
+    alignItems: "center",
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[0]
+        : theme.colors.gray[9],
+    textDecoration: "none",
+    fontSize: 20,
+    fontWeight: 600,
+  },
 }));
 
 export const AppLogo = () => {
-    const { classes, cx } = useStyles();
-    return (<div className={classes.logo}>
-        <ThemeIcon
-            size="xl"
-            radius="md"
-            variant="gradient"
-            style={{ marginRight: 10 }}
-            gradient={{ deg: 0, from: "blue", to: "grape" }}
-        >
-            <IconCoin size={28} stroke={1.5} />
-        </ThemeIcon>
-        DAO History
+  const { classes, cx } = useStyles();
+  return (
+    <div className={classes.logo}>
+      <ThemeIcon
+        size="xl"
+        radius="md"
+        variant="gradient"
+        style={{ marginRight: 10 }}
+        gradient={{ deg: 0, from: "blue", to: "grape" }}
+      >
+        <IconCoin size={28} stroke={1.5} />
+      </ThemeIcon>
+      <Text>DAO History</Text>
     </div>
-    )
-}
-
+  );
+};
