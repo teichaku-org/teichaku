e1aeb51fa70905b78fa18113d71ebf0027aa67e9
kotakamikawa <kamikawat8@gmail.com>
2022-10-24 21:02:18 +0900

    マージンの調整と

--- frontend/src/components/assessment/AssessmentTab.tsx
+++ frontend/src/components/assessment/AssessmentTab.tsx
@@ -1,26 +1,16 @@
-import {
-  Container,
-  Paper,
-  ScrollArea,
-  SimpleGrid,
-  Tabs,
-  Title,
-} from "@mantine/core";
-import {
-  IconPhoto,
-  IconMessageCircle,
-  IconChartLine,
-  IconChartPie3,
-} from "@tabler/icons";
+import { Container, Grid, Loader, Paper, Tabs, Title } from "@mantine/core";
+import { IconChartLine, IconChartPie3 } from "@tabler/icons";
 
 import { DaoHistory } from "@/domains/DaoHistory";
 import { HistoryList } from "../history/HistoryList";
 import { AssessmentBar } from "../graphs/AssessmentBar";
-import { CumulativeReward, RewardHistory } from "../graphs/barTestData";
+import { RewardHistory } from "../graphs/barTestData";
 import { AssessmentLine } from "../graphs/AssessmentLine";
 import { data } from "../graphs/lineTestData";
 import { AssessmentRadar } from "../graphs/AssessmentRadar";
 import { EarnedCoin } from "./EarnedCoin";
+import { useMediaQuery } from "@mantine/hooks";
+import useMetaMask from "@/hooks/web3/useMetaMask";
 
 interface Props {
   daoHistory: DaoHistory[];
@@ -29,57 +19,103 @@ interface Props {
 const AssessmentTab = (props: Props) => {
   const { daoHistory } = props;
 
+  const matches = useMediaQuery("(min-width: 900px)");
+
+  const { address } = useMetaMask();
+  if (!address) {
+    return (
+      <Container>
+        <Loader size="lg" variant="dots" />
+      </Container>
+    );
+  }
+
   //TODO: 自分のアドレスを取得する方法が分からなかったので一旦固定値
   const myDaoHistory = () => {
-    return daoHistory.filter(
-      (dao) => dao.contributor === "0x261f350466E17cbDf9Bc00E2B3875685EF9aB07C"
-    );
+    return daoHistory.filter((dao) => dao.contributor === address);
   };
   return (
     <Tabs defaultValue="total">
       <Tabs.List>
         <Tabs.Tab value="total" icon={<IconChartLine size={14} />}>
-          集計
+          Total
         </Tabs.Tab>
         <Tabs.Tab value="individual" icon={<IconChartPie3 size={14} />}>
-          個別
+          Individual
         </Tabs.Tab>
       </Tabs.List>
 
-      <Tabs.Panel value="individual" pt="xs">
-        <HistoryList data={myDaoHistory()} />
-      </Tabs.Panel>
-
       <Tabs.Panel value="total" pt="xs">
-        <SimpleGrid
-          cols={2}
-          breakpoints={[{ maxWidth: 755, cols: 1, spacing: "sm" }]}
-        >
-          <div>
-            <Title size="h2">合計報酬</Title>
-            <Paper mb="xl" style={{ height: 310 }}>
+        {matches ? (
+          <Grid>
+            <Grid.Col span={4}>
+              <Title mt="md" size="h3">
+                Total Reward
+              </Title>
+              <Paper mt="xs" style={{ height: 310 }}>
+                <EarnedCoin reward={"20000"} />
+              </Paper>
+            </Grid.Col>
+            <Grid.Col span={8}>
+              <Title mt="md" size="h3">
+                Cumulative Reward
+              </Title>
+              <Paper mt="xs" style={{ height: 310 }}>
+                <AssessmentLine data={data} />
+              </Paper>
+            </Grid.Col>
+            <Grid.Col span={4}>
+              <Title mt="md" size="h3">
+                Average Assessment
+              </Title>
+              <Paper mt="xs" style={{ height: 310 }}>
+                <AssessmentRadar data={[]} />
+              </Paper>
+            </Grid.Col>
+            <Grid.Col span={8}>
+              <Title mt="md" size="h3">
+                Reward History
+              </Title>
+              <Paper mt="xs" style={{ height: 310 }}>
+                <AssessmentBar data={RewardHistory} />
+              </Paper>
+            </Grid.Col>
+          </Grid>
+        ) : (
+          <>
+            <Title mt="md" size="h3">
+              Total Reward
+            </Title>
+            <Paper mt="xs" style={{ height: 150 }}>
               <EarnedCoin reward={"20000"} />
             </Paper>
-          </div>
-          <div>
-            <Title size="h2">累積報酬</Title>
-            <Paper mb="xl" style={{ height: 310 }}>
+
+            <Title mt="md" size="h3">
+              Cumulative Reward
+            </Title>
+            <Paper mt="xs" style={{ height: 310 }}>
               <AssessmentLine data={data} />
             </Paper>
-          </div>
-          <div>
-            <Title size="h2">報酬履歴</Title>
-            <Paper style={{ height: 310 }}>
-              <AssessmentBar data={RewardHistory} />
-            </Paper>
-          </div>
-          <div>
-            <Title size="h2">評価集計</Title>
-            <Paper style={{ height: 310 }}>
+
+            <Title mt="md" size="h3">
+              Total Assessment
+            </Title>
+            <Paper mt="xs" style={{ height: 310 }}>
               <AssessmentRadar data={[]} />
             </Paper>
-          </div>
-        </SimpleGrid>
+
+            <Title mt="md" size="h3">
+              Reward History
+            </Title>
+            <Paper mt="xs" style={{ height: 310 }}>
+              <AssessmentBar data={RewardHistory} />
+            </Paper>
+          </>
+        )}
+      </Tabs.Panel>
+
+      <Tabs.Panel value="individual" pt="xs">
+        <HistoryList data={myDaoHistory()} />
       </Tabs.Panel>
     </Tabs>
   );
