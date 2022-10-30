533afb41a13031b27757fcf74940e740917e6a2e
KitaharaMugiro <you@example.com>
2022-10-30 09:06:40 +0900

    言語が切り替わることを確認

--- frontend/src/i18n/locales/en.ts
+++ frontend/src/i18n/locales/en.ts
@@ -1,3 +1,7 @@
 export const en = {
-
+    LP: {
+        HeroText1: 'New DAO Framework to ',
+        HeroText2Colorful: 'Make a Lasting Impact',
+        HeroText3: ' and Retain members.',
+    }
 }
