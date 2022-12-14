3ecbb8b5659332f595314008d36ba2f56cd3a3cd
scalakko <xxx@test.com>
2022-12-15 00:36:57 +0900

    mod:formを使右ように修正

--- frontend/src/components/setting/TokenSetting.tsx
+++ frontend/src/components/setting/TokenSetting.tsx
@@ -2,17 +2,29 @@ import useDaoToken from "@/hooks/dao/useDaoToken";
 import { useLocale } from "@/i18n/useLocale";
 import { Button, Text, Paper, TextInput, Card } from "@mantine/core";
 import { useRouter } from "next/router";
-import {useState} from "react";
+import {useForm} from "@mantine/form";
 
 export const TokenSetting = () => {
   const { t } = useLocale();
   const router = useRouter();
   const { daoId, projectId } = router.query;
   const { tokenSymbol } = useDaoToken({ daoId: daoId as string, projectId: projectId as string });
-  const [value, onChangeText] = useState('');
-  const onClick = () => console.log(value)
 
-    return (
+  const form = useForm({
+    initialValues: {
+      tokenAddress: "",
+    },
+  });
+
+  const update = () => {
+        // const result = [
+        //     form.values.tokenAddress,
+        // ]
+        // const perspectives = result.filter((item) => item !== "" && item !== undefined)
+    console.log(form.values.tokenAddress)
+  }
+
+  return (
     <Paper p="lg" mb="lg">
       <Text size="md" weight={700}>
         {t.Settings.TokenSetting.TokenDistribution}
@@ -23,14 +35,8 @@ export const TokenSetting = () => {
           {tokenSymbol}
         </Text>
       </Card>
-      <TextInput
-          placeholder="0x..."
-          label={t.Settings.TokenSetting.AddressInput.Label}
-          mb="sm"
-          onChange={(event) => onChangeText(event.currentTarget.value)}
-          value={value}
-      />
-      <Button onClick={onClick}>{t.Button.Update}</Button>
+      <TextInput placeholder="0x..." label={t.Settings.TokenSetting.AddressInput.Label} mb="sm" {...form.getInputProps("tokenAddress")}　/>
+      <Button onClick={update}>{t.Button.Update}</Button>
     </Paper>
   );
 };
