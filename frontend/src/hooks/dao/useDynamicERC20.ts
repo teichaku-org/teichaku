ea77cb1429f2dfdccd2a47230f820e0fe4fada31
KitaharaMugiro <you@example.com>
2023-02-17 11:23:24 +0900

    buildエラー修正

--- frontend/src/hooks/dao/useDynamicERC20.ts
+++ frontend/src/hooks/dao/useDynamicERC20.ts
@@ -4,15 +4,13 @@ import { useDynamicERC20Interface } from "./interface/useDynamicERC20Interface";
 import useDynamicERC20Web2 from "./web2/useDynamicERC20Web2";
 import useDynamicERC20Web3 from "./web3/useDynamicERC20Web3";
 
-const useDynamicERC20: useDynamicERC20Interface = (props: {
-    contractAddress: string
-}) => {
+const useDynamicERC20: useDynamicERC20Interface = () => {
     const [isWeb3] = useAtom(Web3FlagAtom)
     const selectStrategy = () => {
         if (isWeb3) {
-            return useDynamicERC20Web3(props)
+            return useDynamicERC20Web3()
         } else {
-            return useDynamicERC20Web2(props)
+            return useDynamicERC20Web2()
         }
     }
     const strategy = selectStrategy()
