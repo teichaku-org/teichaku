df0aa18488828c04c4ee6630de5704ab78f5e110
Rai <raimi.rarara@gmail.com>
2023-02-19 20:18:42 +0900

    DB にdaoinfoを書き込んで読み込むだけ

--- frontend/src/hooks/dao/web2/useDaoExistCheckWeb2.ts
+++ frontend/src/hooks/dao/web2/useDaoExistCheckWeb2.ts
@@ -1,7 +1,23 @@
+import { useRouter } from "next/router";
 import { useDaoExistCheckInterface } from "../interface/useDaoExistCheckInterface";
-
-
+import { useEffect } from "react";
+import { APIClient } from "@/types/APIClient";
 
 export const useDaoExistCheckWeb2: useDaoExistCheckInterface = () => {
+  const router = useRouter();
+  const { daoId, projectId } = router.query;
+  const apiClient = new APIClient();
 
+  useEffect(() => {
+    if (daoId) {
+      // daoが存在しているか確認
+      apiClient.post("/getDaoInfo", { daoId: daoId }).then((res) => {
+        if (!res) {
+          console.log("res: ", res);
+          router.push("/create-dao");
+          return;
+        }
+      });
+    }
+  }, [daoId, router]);
 };
--- frontend/src/hooks/dao/web2/useDaoLauncherWeb2.ts
+++ frontend/src/hooks/dao/web2/useDaoLauncherWeb2.ts
@@ -1,26 +1,55 @@
 import { useLocale } from "@/i18n/useLocale";
 import { useRouter } from "next/router";
 import { useDaoLauncherInterface } from "../interface/useDaoLauncherInterface";
-
+import { hideNotification, showNotification } from "@mantine/notifications";
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
+import { APIClient } from "@/types/APIClient";
 
 const useDaoLauncherWeb2: useDaoLauncherInterface = () => {
-    const router = useRouter()
-    const { t } = useLocale()
+  const router = useRouter();
+  const { t } = useLocale();
+  const apiClient = new APIClient();
 
-    const createDao = async (daoId: string, projectId: string,
-        daoName: string, daoDescription: string,
-        website: string, logo: string,
-        tokenAddress: string,
-        contributorReward: number,
-        reviewerReward: number,
-        votingDurattion: number) => {
+  const createDao = async (
+    daoId: string,
+    projectId: string,
+    daoName: string,
+    daoDescription: string,
+    website: string,
+    logo: string,
+    tokenAddress: string,
+    contributorReward: number,
+    reviewerReward: number,
+    votingDurattion: number
+  ) => {
+    showNotification({
+      id: "createDao",
+      title: t.CreateDao.Complete.Notification.Title,
+      message: t.CreateDao.Complete.Notification.Message,
+      loading: true,
+      autoClose: false,
+    });
 
-    }
+    await apiClient.post("/addDao", {
+      daoId: daoId,
+      projectId: projectId,
+      daoName: daoName,
+      daoDescription: daoDescription,
+      website: website,
+      logo: logo,
+      tokenAddress: tokenAddress,
+      contributorReward: contributorReward,
+      reviewerReward: reviewerReward,
+      votingDurattion: votingDurattion,
+    });
 
-    return {
-        createDao
-    };
+    router.push(`/${daoId}/${projectId}/overview`);
+    hideNotification("createDao");
+  };
+
+  return {
+    createDao,
+  };
 };
 
-
-export default useDaoLauncherWeb2
+export default useDaoLauncherWeb2;
