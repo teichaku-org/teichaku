a128c031c1d53ac4d26666a30eebca497bde2f0a
Rai <raimi.rarara@gmail.com>
2023-02-19 20:17:41 +0900

    APIClient Class 作成

---
+++ frontend/src/types/APIClient.ts
@@ -0,0 +1,72 @@
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
+
+export interface HttpClient {
+  get: (path: string, params: any) => Promise<void | AxiosResponse<any, any>>;
+  post: (path: string, params: any) => Promise<void | AxiosResponse<any, any>>;
+  put: (path: string, params: any) => Promise<void | AxiosResponse<any, any>>;
+  delete: (
+    path: string,
+    params: any
+  ) => Promise<void | AxiosResponse<any, any>>;
+}
+
+export class APIClient implements HttpClient {
+  static baseUrl: string =
+    "https://asia-northeast1-teichaku-fa2a9.cloudfunctions.net";
+
+  public async get(path: string, params: any) {
+    const options: AxiosRequestConfig = {
+      url: APIClient.baseUrl + path,
+      method: "GET",
+      data: params,
+    };
+    const res: AxiosResponse | void = await axios(options).catch(
+      (e: AxiosError<{ error: string }>) => {
+        console.log(e.message);
+      }
+    );
+    return res;
+  }
+
+  public async post(path: string, params: any) {
+    const options: AxiosRequestConfig = {
+      url: APIClient.baseUrl + path,
+      method: "POST",
+      data: params,
+    };
+    const res: AxiosResponse | void = await axios(options).catch(
+      (e: AxiosError<{ error: string }>) => {
+        console.log(e.message);
+      }
+    );
+    return res;
+  }
+
+  public async put(path: string, params: any) {
+    const options: AxiosRequestConfig = {
+      url: APIClient.baseUrl + path,
+      method: "PUT",
+      data: params,
+    };
+    const res: AxiosResponse | void = await axios(options).catch(
+      (e: AxiosError<{ error: string }>) => {
+        console.log(e.message);
+      }
+    );
+    return res;
+  }
+
+  public async delete(path: string, params: any) {
+    const options: AxiosRequestConfig = {
+      url: APIClient.baseUrl + path,
+      method: "DELETE",
+      data: params,
+    };
+    const res: AxiosResponse | void = await axios(options).catch(
+      (e: AxiosError<{ error: string }>) => {
+        console.log(e.message);
+      }
+    );
+    return res;
+  }
+}
