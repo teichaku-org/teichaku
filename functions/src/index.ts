5301b2d78ac1329071ffef3896887667d3467130
Rai <raimi.rarara@gmail.com>
2023-02-21 22:49:44 +0900

    貢献登録して、それを表示するAPI

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,5 +5,7 @@ admin.initializeApp();
 import { createDao } from "./api/createDao";
 import { getDaoInfo } from "./api/getDaoInfo";
 import { getIsWeb3 } from "./api/getIsWeb3";
+import { getPollDetail } from "./api/getPollDetail";
+import { candidateToCurrentPoll } from "./api/candidateToCurrentPoll";
 
-module.exports = { createDao, getDaoInfo, getIsWeb3 };
+module.exports = { createDao, getDaoInfo, getIsWeb3, getPollDetail, candidateToCurrentPoll };
