27fb698a06b91f72ce5c46c58e94b32a5fde39b5
Rai <raimi.rarara@gmail.com>
2023-02-21 22:44:35 +0900

    isWeb3を引数として渡す

--- frontend/src/hooks/dao/useDaoExistCheck.ts
+++ frontend/src/hooks/dao/useDaoExistCheck.ts
@@ -1,21 +1,18 @@
-import { Web3FlagAtom } from "@/domains/atoms/Web3FlagAtom";
-import { useAtom } from "jotai";
-import { useDaoExistCheckInterface } from "./interface/useDaoExistCheckInterface";
-import { useDaoExistCheckWeb2 } from "./web2/useDaoExistCheckWeb2";
-import { useDaoExistCheckWeb3 } from "./web3/useDaoExistCheckWeb3";
+import { Web3FlagAtom } from "@/domains/atoms/Web3FlagAtom"
+import { useAtom } from "jotai"
+import { useDaoExistCheckInterfaceIsWeb3 } from "./interface/useDaoExistCheckInterface"
+import { useDaoExistCheckWeb2 } from "./web2/useDaoExistCheckWeb2"
+import { useDaoExistCheckWeb3 } from "./web3/useDaoExistCheckWeb3"
 
-
-
-export const useDaoExistCheck: useDaoExistCheckInterface = () => {
-    const [isWeb3] = useAtom(Web3FlagAtom)
-    const selectStrategy = () => {
-        if (isWeb3) {
-            return useDaoExistCheckWeb3()
-        } else {
-            return useDaoExistCheckWeb2()
-        }
+export const useDaoExistCheck: useDaoExistCheckInterfaceIsWeb3 = (isWeb3: boolean) => {
+  const selectStrategy = () => {
+    if (isWeb3) {
+      return useDaoExistCheckWeb3()
+    } else {
+      return useDaoExistCheckWeb2()
     }
-    const strategy = selectStrategy()
+  }
+  const strategy = selectStrategy()
 
-    return strategy
-};
+  return strategy
+}
--- frontend/src/hooks/dao/useDaoHistory.ts
+++ frontend/src/hooks/dao/useDaoHistory.ts
@@ -1,16 +1,13 @@
-import { Web3FlagAtom } from "@/domains/atoms/Web3FlagAtom";
-import { useAtom } from "jotai";
-import { useDaoHistoryInterface } from "./interface/useDaoHistoryInterface";
-import useDaoHistoryWeb2 from "./web2/useDaoHistoryWeb2";
-import useDaoHistoryWeb3 from "./web3/useDaoHistoryWeb3";
+import { useDaoHistoryInterfaceIsWeb3 } from "./interface/useDaoHistoryInterface"
+import useDaoHistoryWeb2 from "./web2/useDaoHistoryWeb2"
+import useDaoHistoryWeb3 from "./web3/useDaoHistoryWeb3"
 
 interface Props {
   daoId: string
   projectId: string
 }
 
-const useDaoHistory: useDaoHistoryInterface = (props: Props) => {
-  const [isWeb3] = useAtom(Web3FlagAtom)
+const useDaoHistory: useDaoHistoryInterfaceIsWeb3 = (props: Props, isWeb3: boolean) => {
   const selectStrategy = () => {
     if (isWeb3) {
       return useDaoHistoryWeb3(props)
@@ -21,6 +18,6 @@ const useDaoHistory: useDaoHistoryInterface = (props: Props) => {
   const strategy = selectStrategy()
 
   return strategy
-};
+}
 
 export default useDaoHistory
