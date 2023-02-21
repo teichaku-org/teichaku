c8d2af25b762de03f4cfd995e06d5f4d6cead53f
Rai <raimi.rarara@gmail.com>
2023-02-21 22:44:56 +0900

    add Interface

--- frontend/src/hooks/dao/interface/useDaoHistoryInterface.ts
+++ frontend/src/hooks/dao/interface/useDaoHistoryInterface.ts
@@ -2,12 +2,22 @@ import { Assessment } from "@/domains/Assessment"
 import { DaoHistory } from "@/domains/DaoHistory"
 import { DaoInfo } from "@/domains/DaoInfo"
 
-export type useDaoHistoryInterface = (props: {
+export type useDaoHistoryInterface = (props: { daoId: string; projectId: string }) => {
+  daoHistory: DaoHistory[]
+  assessments: Assessment[]
+  daoInfo: DaoInfo | undefined
+  load: () => Promise<void>
+}
+
+export type useDaoHistoryInterfaceIsWeb3 = (
+  props: {
     daoId: string
     projectId: string
-}) => {
-    daoHistory: DaoHistory[],
-    assessments: Assessment[],
-    daoInfo: DaoInfo | undefined,
-    load: () => Promise<void>
+  },
+  isWeb3: boolean
+) => {
+  daoHistory: DaoHistory[]
+  assessments: Assessment[]
+  daoInfo: DaoInfo | undefined
+  load: () => Promise<void>
 }
--- frontend/src/hooks/dao/interface/useDaoLoadInterface.ts
+++ frontend/src/hooks/dao/interface/useDaoLoadInterface.ts
@@ -1,1 +1,3 @@
 export type useDaoLoadInterface = () => void
+
+export type useDaoLoadInterfaceIsWeb3 = (isWeb3: boolean) => void
