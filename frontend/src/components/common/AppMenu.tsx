ff9f70bd07088e0a00df948e80ab80ec755319f4
KitaharaMugiro <you@example.com>
2023-02-26 18:15:01 +0900

    アドレスを表示させた

--- frontend/src/components/common/AppMenu.tsx
+++ frontend/src/components/common/AppMenu.tsx
@@ -1,19 +1,22 @@
-import { useLocale } from "@/i18n/useLocale";
-import { Button, Menu } from "@mantine/core";
-import { NavbarLinks } from "./NavbarLinks";
+import useWeb3Auth from "@/hooks/web3/useWeb3Auth"
+import { useLocale } from "@/i18n/useLocale"
+import { shortenAddress } from "@/utils/shortenAddress"
+import { Button, Menu } from "@mantine/core"
+import { NavbarLinks } from "./NavbarLinks"
 
 export const AppMenu = () => {
-  const { t } = useLocale();
+  const { t } = useLocale()
+  const { address } = useWeb3Auth()
   return (
     <Menu trigger="hover" openDelay={100} closeDelay={400}>
       <Menu.Target>
         <Button variant="gradient" gradient={{ from: "blue", to: "grape" }}>
-          {t.Button.AppMenu}
+          {shortenAddress(address)}
         </Button>
       </Menu.Target>
       <Menu.Dropdown>
         <NavbarLinks />
       </Menu.Dropdown>
     </Menu>
-  );
-};
+  )
+}
