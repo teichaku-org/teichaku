7b53497d765206cfb09600780bf83ead901c0314
Rai <raimi.rarara@gmail.com>
2023-03-06 18:17:35 +0900

    font

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,20 +1,27 @@
-import { createGetInitialProps } from "@mantine/next";
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import { createGetInitialProps } from "@mantine/next"
+import Document, { Head, Html, Main, NextScript } from "next/document"
 
-const getInitialProps = createGetInitialProps();
+const getInitialProps = createGetInitialProps()
 
 export default class _Document extends Document {
-  static getInitialProps = getInitialProps;
+  static getInitialProps = getInitialProps
 
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&family=Nunito&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
